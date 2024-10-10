78b7dbf0efb25b4c02cb89c54fef6b5b060c887e
moti <mordechayamramy@gmail.com>
2024-10-10 15:35:49 +0300

    create a period_area_blueprint

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from controllers.accidents_area_controller import accidents_area_blueprint
 from controllers.injuries_area_controller import injuries_area_blueprint
 from controllers.accidents_cause_controller import accidents_cause_blueprint
 # from controllers.accidents_init import
-from controllers.period_area_controller import
+from controllers.period_area_controller import period_area_blueprint
 from flask import Flask
 
 
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.register_blueprint(accidents_area_blueprint, url_prefix='/api')
 app.register_blueprint(injuries_area_blueprint, url_prefix='/api')
 app.register_blueprint(accidents_cause_blueprint, url_prefix='/api')
-app.register_blueprint(accidents_cause_blueprint, url_prefix='/api/cars')
+app.register_blueprint(period_area_blueprint, url_prefix='/api/cars')
 # app.register_blueprint(accidents_cause_blueprint, url_prefix='/api/cars')
 
 if __name__ == '__main__':
--- controllers/period_area_controller.py
+++ controllers/period_area_controller.py
@@ -1,27 +1,21 @@
 from datetime import datetime, timedelta
 
 from flask import Blueprint, jsonify, request
-from database.connect import accidents_collection
-
+from database.connect import accidents_collection, daily_collection
 
 period_area_blueprint = Blueprint('injuries_area', __name__)
 
 
 @period_area_blueprint.route('/accidents/area/<area>/period', methods=['GET'])
 def get_accidents_by_area_and_period(area):
-    period = request.args.get('period', 'day')
-    start_date = request.args.get('start_date')
-    end_date = request.args.get('end_date')
+    start_date = request.args.get('period')
 
     start_date = datetime.strptime(start_date, '%Y-%m-%d')
-    end_date = datetime.strptime(end_date, '%Y-%m-%d')
 
     query = {
         'BEAT_OF_OCCURRENCE': area,
-        'CRASH_DATE': {
-            '$gte': start_date,
-            '$lt': end_date + timedelta(days=1)  # Include end date
-        }
+        'CRASH_DATE': start_date
     }
-    accidents = list(accidents_collection.find(query))
+
+    accidents = list(daily_collection.find(query))
     return jsonify(accidents), 200
